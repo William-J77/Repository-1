3c58b4ad93ce6680e0cd7d4ad308026657e9ec4a
William-J77 <Willj77@gmail.com>
2025-06-17 15:16:11 -0700

    Guessing game and Name Asking

---
+++ Name asking.py
@@ -0,0 +1,10 @@
+name = input("What is your name? ").title()
+puter = ("I am a puter") 
+print("Hello, " + name + "!")
+if name == ("William"):
+    print("Oh! It's you! William!.")
+if name == ("Jimbo"):
+    print("Jimbo Jones?")
+print("Nice to meet you " + name + "!")
+print(puter)
+
