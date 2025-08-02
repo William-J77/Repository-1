164516075233cca82cd06544110fde268f7d6021
William-J77 <Willj77@gmail.com>
2025-08-01 20:24:26 -0700

    080125

---
+++ weapon stats dict.py
@@ -0,0 +1,47 @@
+
+# weapon_stats = {"damage": 10}
+
+# def update_weapons_stats(stats, key, value):
+#     stats[key] = value
+
+# update_weapons_stats(weapon_stats, "range", 5)
+# print(weapon_stats)
+
+
+
+# spell_stats = {
+#     "power": 12,
+#     "mana_cost": 8
+# }
+
+# def update_stats(spell_stats, key, value):
+#     spell_stats[key] = value
+
+# update_stats(spell_stats, "cooldown", 3)
+# update_stats(spell_stats, "agility", 6)
+
+# # Add your new stat updates here:
+# # Example:
+# # key = "cooldown"
+# # value = 3
+# # spell_stats[key] = value
+
+# # Then print the final spell_stats dictionary:
+# print()
+# print(spell_stats)
+
+spell_stats = {
+    "power": 12,
+    "mana_cost": 8
+}
+
+def safe_add_stat(spell_stats, key, value):
+    if key in spell_stats:
+        print(f"Stat {key} already exists with value {spell_stats[key]}.")
+    else:
+        spell_stats[key] = value
+
+safe_add_stat(spell_stats, "agility", 8)
+
+print()
+print(spell_stats)
